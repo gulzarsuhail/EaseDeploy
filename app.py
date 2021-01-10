9b79c79f583e4d3c406d94dad5cb21e527750b2c
gulzarsuhail <suhail.gulzar95@gmail.com>
2021-01-10 18:33:26 +0530

    POC working 🎉🎉

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import hjson
+import joblib
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+# read and parse the config.hjson file
+config = hjson.loads(open('config.hjson', 'r').read())
+model = joblib.load(config['joblibFilePath'])
+
+@app.route('/', methods=['GET'])
+def sendHomepage():
+    return render_template('index.html', config=config)
+
+@app.route('/', methods=['POST'])
+def predict():
+    model = joblib.load(config['joblibFilePath'])
+
+if __name__ == '__main__':
+    app.run()
