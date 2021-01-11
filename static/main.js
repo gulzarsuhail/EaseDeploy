b98f5030d0a809b09933d3c217241aa0c79ff957
gulzarsuhail <suhail.gulzar95@gmail.com>
2021-01-11 13:07:37 +0530

    move main.js into html, preventing  GET request

--- static/main.js
+++
@@ -1,7 +0,0 @@
-// To enable dropdown function
-// P.S I hate jquery -_-
-$('.ui.selection.dropdown').dropdown({});
-
-// Enable validation
-// P.S I still hate jquery -_-
-$('.ui.form').form({ fields: jsValidation });
